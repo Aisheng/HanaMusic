5646e476cb016131cbbd7c11ebcb8e963db63bc9
Aisheng <Lidaisheng7142@163.com>
2017-03-29 01:17:19 +0800

    commit

---
+++ app/src/main/java/com/example/aisheng/hanamusic/activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.example.aisheng.hanamusic.activity;
+
+import android.content.ServiceConnection;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Aisheng on 2017/3/29.
+ */
+
+public class BaseActivity extends AppCompatActivity implements ServiceConnection {
+
+
+
+    
+
+
+}
--- app/src/main/java/com/example/aisheng/hanamusic/activity/MainActivity.java
+++ app/src/main/java/com/example/aisheng/hanamusic/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.aisheng.hanamusic;
+package com.example.aisheng.hanamusic.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.aisheng.hanamusic.R;
 
 public class MainActivity extends AppCompatActivity {
 
