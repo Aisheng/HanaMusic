ebcec84233b207778819a3bfe811f832effe8f76
Aisheng <Lidaisheng7142@163.com>
2017-04-02 23:35:04 +0800

    TabActivity

--- app/src/main/java/com/example/aisheng/hanamusic/activity/TabActivity.java
+++ app/src/main/java/com/example/aisheng/hanamusic/activity/TabActivity.java
@@ -1,7 +1,29 @@
 package com.example.aisheng.hanamusic.activity;
 
+
+import android.os.Bundle;
+
+import com.example.aisheng.hanamusic.R;
+
 /**
  * Created by Administrator on 2017/3/31.
  */
-public class TabActivity {
+public class TabActivity extends BaseActivity {
+
+    private int page, artistId, albumId;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        if (getIntent().getExtras() != null) {
+            page = getIntent().getIntExtra("page_number", 0);
+            artistId = getIntent().getIntExtra("artist", 0);
+            albumId = getIntent().getIntExtra("album", 0);
+        }
+        setContentView(R.layout.activity_tab);
+
+
+    }
+
 }
